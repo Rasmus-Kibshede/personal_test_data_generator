b40aae0f97b2fd65bee41bfa672cd66d570d6f44
Rasmus Kibshede <2740ralle@gmail.com>
2023-10-09 22:37:15 +0200

    added: jest config file

--- test/UnitTest/personService.test.ts
+++ test/UnitTest/personService.test.ts
@@ -1,11 +1,17 @@
+import { Person } from '../../src/Model/Person';
 import * as personService from '../../src/Services/personService';
 
 describe('first test ever made in the world.', () => {
     //arrange
-    const testData = [{ personId: 1, Person: { fullName: 'Jens Jensen', gender: true } }]
+    const testData = [{ personId: 1, fullName: 'Jens Jensen', gender: true }]
     //act
-    test.each(testData)('test correct name', ({ personId, Person }) => {
-        expect(personService.getNameGender(personId)).toBe(Person)
+    test.each(testData)('test correct name', async ({ personId, fullName, gender }) => {
+        const response = await personService.getNameGender(personId) as Person;
+        console.log(response);
+        console.log(fullName);
+        
+        
+        expect(response.fullName).toBe(fullName);
     })
     //Assert
 })
