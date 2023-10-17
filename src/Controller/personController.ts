33be4a457023933e0058116b394637e58b95af37
Rasmus Kibshede <2740ralle@gmail.com>
2023-10-17 15:20:32 +0200

    change: unpacked promise for getNameGender

--- src/Controller/personController.ts
+++ src/Controller/personController.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from 'express';
-import * as personService from '../Services/personService'; 	
+import * as personService from '../Services/personService';
 import { PersonDTO } from '../Model/PersonDTO';
 
 export const getNameGender = async (req: Request, res: Response) => {
-    const response = await personService.getNameGender(Number(req.params.id));
-	
-    personResponse(response ? response : { err: response }, res, 200);
+	const response = await personService.getNameGender(Number(req.params.id));
+
+	if (!response) {
+		res.status(404).send({ err: response });
+	} else {
+		personResponse(response ? response : { err: response }, res, 200);
+	}
 };
 
 const personResponse = (response: PersonDTO | { err: string }, res: Response, statusCode: number) => {
