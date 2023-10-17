431e2894fe80ee30be00ecea1dd7abf60b710d22
Rasmus Kibshede <2740ralle@gmail.com>
2023-10-17 15:12:31 +0200

    change: unpacked promise for getNameGender

--- src/Services/personService.ts
+++ src/Services/personService.ts
@@ -2,5 +2,8 @@ import { resolve } from 'path';
 import { getNameGender as repoGetNameGender } from '../Repositories/personRepository';
 
 export const getNameGender = async (id: number) => {
-    return repoGetNameGender(id);
+
+    const t = await repoGetNameGender(id);
+
+    return t ? t : null;
 };
