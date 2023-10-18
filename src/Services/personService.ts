a709054409b7544616d94f3e215257e600fbfb57
Rasmus Kibshede <2740ralle@gmail.com>
2023-10-18 13:47:30 +0200

    added: test for fullname and gender with mocks

--- src/Services/personService.ts
+++ src/Services/personService.ts
@@ -1,6 +1,5 @@
 import { PersonDTO1, PersonDTO2 } from "../Model/PersonDTO";
 import { getAllPersonsFromFile } from "../Repositories/fileHandler";
-import { Person } from "../Model/Person";
 
 export const getRandomNameAndGender = async () => {
 
@@ -16,7 +15,6 @@ export const getRandomNameAndGender = async () => {
     const randomPerson = allPersons[randomNumber];
 
     const fullname = randomPerson.name + ' ' + randomPerson.surname;
-    // const person = new Person(12345678, fullname, 'male', '18-10-2023', 'address', 87654321);
 
     const person: PersonDTO2 = {
         fullname: fullname,
--- test/UnitTest/personService.test.ts
+++ test/UnitTest/personService.test.ts
@@ -1,18 +1,97 @@
 import 'dotenv/config';
 import { getRandomNameAndGender } from '../../src/Services/personService';
+import { PersonDTO1, PersonDTO2 } from '../../src/Model/PersonDTO';
+let persons: PersonDTO1[];
 
-
-test('Testing FullName', async () => {
-    const person = await getRandomNameAndGender();
-
-    expect(person.fullname.length).toBeGreaterThan(0);
-    expect(person.gender.length).toBeGreaterThan(0);
-
-    expect(person.fullname.length).toBeLessThan(2147483647);
-    expect(person.gender.length).toBeLessThan(2147483647);
-
-    expect(['male', 'female']).toContain(person.gender);
-    expect(person.fullname).toContain(' ');
-
-    expect(person.fullname).toMatch(/^[a-zA-Z\s]*$/);
+jest.mock("../../src/Repositories/fileHandler", () => {
+    const originalModule = jest.requireActual("../../src/Repositories/fileHandler");
+    return {
+        getAllPersonsFromFile: jest.fn(originalModule).mockImplementation(() => Promise.resolve(
+            persons
+        ))
+    };
 });
+
+let person: PersonDTO2;
+
+
+beforeEach(async () => {
+    person = await getRandomNameAndGender();
+});
+
+describe('', () => {
+    persons = [
+        {
+            name: 'Annemette P.',
+            surname: 'Nilsson',
+            gender: 'female'
+        },
+        {
+            name: "Lucas M.",
+            surname: "Kjær",
+            gender: "male"
+        }
+    ];
+
+
+    describe('Fullname length passes', () => {
+        test('length is less then max string length', async () => {
+            expect(person.fullname.length).toBeLessThan(2147483647);
+        });
+
+        test('length is greater then 0', async () => {
+            expect(person.fullname.length).toBeGreaterThan(0);
+        });
+    });
+
+    describe('Gender length passes', () => {
+        test('length is less then max string length', async () => {
+            expect(person.gender.length).toBeLessThan(2147483647);
+        });
+
+        test('length is greater then 0', async () => {
+            expect(person.gender.length).toBeGreaterThan(0);
+        });
+    });
+
+    describe('Fullname format passes', () => {
+        test('Fullname contains a space', () => {
+            expect(person.fullname).toContain(' ');
+        });
+
+        test('Fullname matches alphabet chars with space', () => {
+            expect(person.fullname).toMatch(/^[a-æA-Æ\sa.c]*$/);
+        });
+    });
+
+    describe('Gender format passes', () => {
+        test('Gender contains male or female passes', async () => {
+
+            expect(['male', 'female']).toContain(person.gender);
+        });
+
+        test('Gender matches alphabet chars', () => {
+            expect(person.gender).toMatch(/^[a-mA-M]*$/);
+        });
+    });
+});
+
+// describe('', () => {
+//     persons = [
+//         {
+//             name: 'Anneme9tte P.',
+//             surname: 'Nilsson',
+//             gender: 'female'
+//         },
+//         {
+//             name: "Lucas M,",
+//             surname: "Kj2ær",
+//             gender: "male"
+//         },
+//         {
+//             name: "LucÅs M.",
+//             surname: "Kj2ær",
+//             gender: "mal10e"
+//         }
+//     ];
+// });
