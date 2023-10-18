e6ec58076ffe79cf122e5cde3ecf0ef6704cdfad
Rasmus Kibshede <2740ralle@gmail.com>
2023-10-18 09:44:53 +0200

    moved: filehandler to own file

--- src/Services/personService.ts
+++ src/Services/personService.ts
@@ -1,25 +1,17 @@
-import fs from 'fs';
-import { PersonDTO } from '../Model/PersonDTO';
+import { getAllPersonsFromFile } from "../Repositories/fileHandler";
 
 export const getRandomNameAndGender = async () => {
 
     const allPersons = await getAllPersonsFromFile();
+
+    if (!allPersons) {
+        throw new Error('No persons found');
+    }
+
 
     Math.random();
 
     const randomNumber = Math.floor(Math.random() * allPersons.length);
 
     return allPersons[randomNumber];
-};
-
-const getAllPersonsFromFile = async () => {
-    let fileContent = fs.readFileSync('./documents/person-names.json', 'utf16le');
-
-    // Remove any leading Byte Order Mark (BOM), only ussed in UFT-16 and UFT-32
-    if (fileContent.charCodeAt(0) === 0xfeff) {
-        fileContent = fileContent.slice(1);
-    }
-
-    const data: PersonDTO[] = JSON.parse(fileContent)[0].persons;
-    return data;
 };
