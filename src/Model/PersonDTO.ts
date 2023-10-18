f8101f575d786d33afae946f770e07a678bd1daa
Rasmus Kibshede <2740ralle@gmail.com>
2023-10-18 09:23:10 +0200

    ongoing: name gender

--- src/Model/PersonDTO.ts
+++ src/Model/PersonDTO.ts
@@ -14,6 +14,6 @@ interface Person {
 }
 
 interface PersonRequestDTO {
-	email: string;
-	password: string;
+    email: string;
+    password: string;
 }
--- src/Services/personService.ts
+++ src/Services/personService.ts
@@ -6,8 +6,9 @@ export const getRandomNameAndGender = async () => {
     const allPersons = await getAllPersonsFromFile();
 
     Math.random();
-
     const randomNumber = Math.floor(Math.random() * allPersons.length);
+
+    const randomPerson: PersonDTO = allPersons[randomNumber];
 
     return allPersons[randomNumber];
 };
