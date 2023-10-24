c37815011249c6f8092e748fb0098b60390e21cb
Thony <thon0101@stud.kea.dk>
2023-10-24 13:24:37 +0200

    wp

--- test/UnitTest/personService.test.ts
+++ test/UnitTest/personService.test.ts
@@ -3,7 +3,11 @@ import { randomNumberPrefix, generateRandomDigits } from "../../src/Services/per
 
 let prefix: string;
 let randomDigits: string;
-
+const prefixes = [
+  {'number': '2', 'expected': 7},
+  {'number': '30', 'expected': 6},
+  {'number': '342', 'expected': 5},
+  ];
 
 beforeAll(async () => {
     prefix = await randomNumberPrefix();
@@ -14,30 +18,27 @@ beforeAll(async () => {
 
 describe("testing generateFakeMobilePhoneNumber", () => {
     const phoneNumberPrefixes = [
-        '2', '30', '31', '40', '41', '42', '50', '51', '52', '53',
-        '60', '61', '71', '81', '91', '92', '93', '342', '344', '345',
-        '346', '347', '348', '349', '356', '357', '359', '362', '365',
-        '366', '389', '398', '431', '441', '462', '466', '468', '472',
-        '474', '476', '478', '485', '486', '488', '489', '493', '494',
-        '495', '496', '498', '499', '542', '543', '545', '551', '552',
-        '556', '571', '572', '573', '574', '577', '579', '584', '586',
-        '587', '589', '597', '598', '627', '629', '641', '649', '658',
-        '662', '663', '664', '665', '667', '692', '693', '694', '697',
-        '771', '772', '782', '783', '785', '786', '788', '789', '826',
-        '827', '829',
+        
     ];
 
     describe("testing valid prefix", () => {
         test("should be bigger than 0", () => {
-            expect(prefix).toBeGreaterThan(0);
+            expect(prefix.length).toBeGreaterThan(0);
         });
 
         test("should be less than 9", () => {
-            expect(prefix).toBeLessThanOrEqual(3);
+            expect(prefix.length).toBeLessThanOrEqual(3);
         });
     });
 
-    describe('Testing valid digtigts from all prefixes', () => {
+    describe("Testing valid digtigts from all prefixes", () => {
 
     })
+
+    describe("Testing length of randomDigits", () => {
+        test.each(prefixes)('testing length',async ({ number, expected }) => {
+          let randomDigits = await generateRandomDigits(number.length);
+          expect(randomDigits.length).toBe(expected); 
+        })
+      })
 });
