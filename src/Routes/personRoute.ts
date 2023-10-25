eb017b977d5306c31cfae560cad9fd0db8f1efcb
Rasmus Kibshede <2740ralle@gmail.com>
2023-10-25 20:03:53 +0200

    code cleanup

--- src/Routes/personRoute.ts
+++ src/Routes/personRoute.ts
@@ -1,20 +1,7 @@
-import { NextFunction, Router, Request, Response } from 'express';
+import { Router } from 'express';
 import * as personController from '../Controller/personController'
 
 const personRouter = Router();
-
-//Middleware
-const validateId = (req: Request, res: Response, next: NextFunction) => {
-    const id = req.params.id;
-
-    if (!id) {
-        res.status(400).send({ err: 'Invalid ID' });
-    } else if (isNaN(Number(id))) {
-        res.status(406).send({ err: 'Not Acceptable' });
-    }
-
-    next();
-};
 
 //Endpoints
 personRouter.get('/person/namegender', personController.generateNameAndGender)
@@ -24,6 +11,5 @@ personRouter.get('/person/birthday/', personController.getRandomBirthday)
 personRouter.get('/person/cpr/', personController.getRandomCpr)
 personRouter.get('/person', personController.getPerson)
 personRouter.get('/persons', personController.getPersons)
-
 
 export default personRouter;
