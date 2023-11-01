39500102e016b259374d364d35c136ed20e85465
Rasmus Kibshede <2740ralle@gmail.com>
2023-11-01 21:35:00 +0100

    trigger scanner

--- src/Repositories/fileHandler.ts
+++ src/Repositories/fileHandler.ts
@@ -5,12 +5,12 @@ const personNamePath = './documents/person-names.json';
 
 export const getAllPersonsFromFile = async () => {
 
-    let fileContent = fs.readFileSync(personNamePath, 'utf16le');
+    let fileContent = fs.readFileSync(personNamePath, 'utf-8');
 
     // Remove any leading Byte Order Mark (BOM), only ussed in UFT-16 and UFT-32 file encoding
-    if (fileContent.charCodeAt(0) === 0xfeff) {
-        fileContent = fileContent.slice(1);
-    };
+    // if (fileContent.charCodeAt(0) === 0xfeff) {
+    //     fileContent = fileContent.slice(1);
+    // };
 
     const data: PersonDTOFileObject[] = JSON.parse(fileContent)[0].persons;
     return data;
