01889363a4fcb6baf19fa059bc5412f2c91013c4
Roed <Geofery10@hotmail.com>
2023-12-22 20:33:34 +0100

    Added BaseError, errorHandler and responseController

---
+++ src/Controller/responseController.ts
@@ -0,0 +1,18 @@
+import { BaseError } from '../util/validation/BaseError';
+import { Result, ApiResponse } from '../util/errorHandler';
+import { Response } from 'express';
+
+export const response = (res: Response<unknown, Record<string, unknown>>, response: Result<ApiResponse, BaseError>, statusCodeSuccess: number) => {
+    res.status(response.success ? statusCodeSuccess : Number(response.error.statusCode)).send(response.success ? 
+        { success: response.success, data: response.result.data } : { success: response.success, message: response.error.message });
+};
+
+export const responseNested = (res: Response<unknown, Record<string, unknown>>, response: Result<ApiResponse, BaseError>, statusCodeSuccess: number) => {
+    res.status(response.success ? statusCodeSuccess : Number(response.error.statusCode)).send(response.success ? 
+        { success: response.success, data: (response.result.data as never)[0]} : { success: response.success, message: response.error.message });
+};
+
+export const responseError = (res: Response<unknown, Record<string, unknown>>, response: Result<ApiResponse, BaseError>) => {
+    res.status(response.success ? 400 : Number(response.error.statusCode)).send(response.success ? 
+        { success: response.success, data: response.result.data } : { success: response.success, message: response.error.message });
+};
---
+++ src/util/errorHandler.ts
@@ -0,0 +1,89 @@
+import { BaseError } from "./validation/BaseError";
+
+export type Result<T, E extends BaseError = BaseError> = { success: true, result: T } | { success: false, error: E }
+export type ApiResponse = { data: NonNullable<unknown> }
+
+
+export const ensureError = (value: unknown): Error => {
+    if (value instanceof Error) return value;
+
+    let stringified = '[Unable to stringify the thrown value]';
+    try {
+        stringified = JSON.stringify(value);
+    } catch { /* empty */ }
+
+    const error = new Error(`This value was thrown as is, not through an Error: ${stringified}`);
+    return error;
+};
+
+export const success = (response: NonNullable<unknown>): Result<ApiResponse, BaseError> => {
+    if (Array.isArray(response)) {
+        return { success: true, result: { data: response } };
+    } else {
+        return { success: true, result: { data: response } };
+    }
+};
+
+export const failed = (arg: string | Error): Result<ApiResponse, BaseError> => {
+    if (arg instanceof Error) {
+        return autoError(arg);
+    } else {
+        return customError(arg);
+    }
+};
+
+export const invalidIdError = (entityName: string) => {
+    if (entityName === 'user') {
+        return new Error(`No ${entityName} with that email`);
+    }
+    return new Error(`No ${entityName} with that id`);
+};
+
+export const autoError = (arg: Error): Result<ApiResponse, BaseError> => {
+    const error = ensureError(arg);
+    let statusCode: string;
+
+    if ('code' in error) {
+        statusCode = generateStatusCode(String(error.code));        
+        return {
+            success: false, error: new BaseError(error.message, {
+                error: error,
+                statusCode: statusCode
+            })
+        };
+    } 
+    return {
+        success: false, error: new BaseError(error.message, {
+            error: error,
+            statusCode: generateStatusCode(error.message)
+        })
+    };
+};
+
+export const customError = (arg: string): Result<ApiResponse, BaseError> => {
+    const error = invalidIdError(arg);
+    return {
+        success: false, error: new BaseError(error.message, {
+            error: error,
+            statusCode: generateStatusCode('with that id')
+        })
+    };
+};
+
+export const generateStatusCode = (err: string): string => {
+    const errorMappings: Record<string, string> = {
+        'Invalid credentials': '400',
+        'Invalid datel': '400',
+        'Invalid Email': '400',
+        'Not Acceptable': '406',
+        'Invalid ID': '400',
+        'with that id': '404',
+        'ER_BAD_FIELD_ERROR': '404',
+        'ER_DUP_ENTRY': '409',
+    };
+    const statusCode = errorMappings[err] || '500';
+    return statusCode;
+};
+
+// TODO: Add error handler for mongoDB errors
+// E11000 duplicate key error collection: test.role_mdb index: UQ_b3c70d0c75c21d7ed9adfeea828 dup key: { title: \"Super dooper cooper admin\" }
