21c3ac1b2704752b49c5f4eeb160171f9b75dab1
Roed <Geofery10@hotmail.com>
2023-12-25 23:01:29 +0100

    minor fix

--- src/Routes/carRoute.ts
+++ src/Routes/carRoute.ts
@@ -5,7 +5,7 @@ const carRouter = Router();
 
 //Endpoints
 carRouter.get('/car', carController.generateCar)
-//ID er antallet af biler man vil have genereret. 
+//count er antallet af biler man vil have genereret. 
 carRouter.get('/cars/:count', carController.generateCars)
 carRouter.get('/car/:id', carController.getCarById)
 
