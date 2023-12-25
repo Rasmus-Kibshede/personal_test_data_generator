dd9651fcd8608728c898920a4530dc0c54467fe7
Roed <Geofery10@hotmail.com>
2023-12-25 22:58:47 +0100

    removed more comments

--- src/Routes/carRoute.ts
+++ src/Routes/carRoute.ts
@@ -8,9 +8,5 @@ carRouter.get('/car', carController.generateCar)
 //ID er antallet af biler man vil have genereret. 
 carRouter.get('/cars/:count', carController.generateCars)
 carRouter.get('/car/:id', carController.getCarById)
-/*carRouter.get('/car', carController.getRandomBirthday)
-carRouter.get('/car', carController.getRandomCPR)
-carRouter.get('/car', carController.getPerson)
-carRouter.get('/car', carController.getPersons)*/
 
 export default carRouter;
