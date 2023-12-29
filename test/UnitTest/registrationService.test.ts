3ede670677f2455726d2106b7e5280cb6db857d2
Roed <Geofery10@hotmail.com>
2023-12-29 14:21:27 +0100

    minor changes registrationService

--- test/UnitTest/registrationService.test.ts
+++ test/UnitTest/registrationService.test.ts
@@ -2,26 +2,27 @@ import * as registrationService from '../../src/Services/registrationService';
 
 let plate: string;
 let vin: string;
+
 const plates = [
   'AB 34 567', 'CD 56 789', 'EF 78 901', 'GH 90 123', 'IJ 12 345', 'KL 23 456', 'MN 45 678',
   'OP 67 890', 'QR 89 012', 'ST 01 234', 'UV 12 345', 'WX 34 567', 'YZ 56 789', 'AA 78 901',
   'BB 90 123', 'CC 12 345', 'DD 23 456', 'EE 45 678', 'FF 67 890', 'GG 89 012',
 ];
+
 const vinNumbers = [
   '1GNEK13ZX3R298984', '2HNYD28678H532126', '3FAKP0JA2AR245875', '4T1BG22KX2U545767', '5NPD84LF4LH563295',
   '6YV1A22B575218475', '7FARW2H51KE029243', '8G1ND52J636100920', '9BWDE61J334015650', 'JS1GN7EA862101994',
   'KNMAT2MV5KP509788', 'LFGTCKPMXBP008810', 'MHFSA03J0KN009465', 'NMTKHMBX9JR044186', '5UXKR2C58G0R70603',
   'WAUBFAFL7BA003901', 'XWBEN10EX9VS74136', 'YV1RS592672591003', 'ZFA25000001692412',
 ];
-
+//FIX CONST I GENERATE
+//Auto generate vin numbers og plader i service laget? 
 /* ---------------------------------------- generateLicenseNumber ---------------------------------------- */
 describe('generateLicenseNumber', () => {
 
   beforeEach(() => {
     plate = registrationService.generateLicenseNumber();
   });
-
-  describe('Testing valid data in generateLicenseNumber', () => {
 
     test('String validation', () => {
       expect(typeof plate).toBe('string');
@@ -44,7 +45,7 @@ describe('generateLicenseNumber', () => {
       expect(plate).not.toMatch(/[^A-Z\d\s]/);
     });
 
-    test('License number within expected range', () => {
+    test('License number within expected list', () => {
       expect(plates).toContain(plate);
     });
 
@@ -76,7 +77,6 @@ describe('generateLicenseNumber', () => {
       expect(plate).toMatch(/^[a-zA-Z0-9\s]+$/);
     });
   });
-});
 
 /* ---------------------------------------- generateVIN ---------------------------------------- */
 describe('generateVIN', () => {
@@ -93,8 +93,12 @@ describe('generateVIN', () => {
     expect(vin).not.toBe('');
   });
 
-  test('VIN is not null or undefined', () => {
+  test('VIN is not undefined', () => {
     expect(vin).toBeDefined();
+  });
+
+  test('VIN is not undefined', () => {
+    expect(vin).not.toBeNull();
   });
 
   test('VIN contains only alphanumeric characters', () => {
